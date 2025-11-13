dae5d22d819e0cbdbe262e82cee582d952f45274
Lakshmi711git <lakshmihs0711@gmail.com>
2025-11-13 20:01:06 +0530

    Time: 1 ms (99.54%), Space: 57.1 MB (64.66%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+     int max= nums[0];
+     int cur=0;
+     for(int i=0;i<nums.length;i++)
+     {
+        cur+=nums[i];
+        if(cur>max)
+        {
+            max=cur;
+        }
+        if(cur<0)
+        {
+            cur=0;
+
+        }
+        }
+        return max;
+        }}
