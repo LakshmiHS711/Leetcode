cd909177266f3d0da37b5ff0fbe7566589d4e452
LakshmiHS711 <lakshmihs0711@gmail.com>
2025-11-20 20:33:57 +0530

    Time: 16 ms (82.16%), Space: 50.7 MB (24.82%) - LeetHub

--- 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.java
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.java
@@ -1,32 +1,13 @@
-public class Solution {
+class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-        // Initialize count of subarrays, prefix sum, and hash map for remainders
-        int count = 0;
-        int prefixSum = 0;
-        HashMap<Integer, Integer> prefixMap = new HashMap<>();
-        prefixMap.put(0, 1); // To handle subarrays that start from the beginning
-
-        for (int num : nums) {
-            // Update prefix sum
-            prefixSum += num;
-            
-            // Calculate the remainder of the prefix sum divided by k
-            int mod = prefixSum % k;
-            
-            // Adjust negative remainders to be positive
-            if (mod < 0) {
-                mod += k;
-            }
-            
-            // If this remainder has been seen before, it means there are subarrays ending here that are divisible by k
-            if (prefixMap.containsKey(mod)) {
-                count += prefixMap.get(mod);
-                prefixMap.put(mod, prefixMap.get(mod) + 1);
-            } else {
-                prefixMap.put(mod, 1);
-            }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0, remainder = 0; i < nums.length; i++) {
+            remainder = ((remainder + nums[i]) % k + k) % k;
+            map.put(remainder, map.getOrDefault(remainder, 0) + 1);
         }
-        
-        return count; // Total number of subarrays divisible by k
+        int result = map.getOrDefault(0, 0);
+        for (int frequency : map.values())
+            result += frequency * (frequency - 1) / 2;
+        return result;
     }
 }
